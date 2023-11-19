957fcec1b96ab88c06fad11085e976b51c56fb03
Joshua Morony <joshua.morony@gmail.com>
2023-11-19 19:57:10 +1030

    fix: add RouterLink

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.css'
+  styleUrl: './navigation.component.css',
 })
-export class NavigationComponent {
-
-}
+export class NavigationComponent { }
